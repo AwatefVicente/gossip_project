5eadd5b9d72d85f0e3028089196ae1837e4526de
AwatefVicente <vawatef@hotmail.fr>
2022-10-27 21:50:45 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,44 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  tag = Tag.create!(
+    title: Faker::Books::CultureSeries.civ,
+    gossip_id: rand(1..20)
+  )
+end
+
+10.times do
+  user = User.create!(
+    first_name: Faker::TvShows::GameOfThrones.dragon,
+    last_name: Faker::Name.last_name,
+    description: Faker::Movies::StarWars.quote,
+    email: Faker::TvShows::GameOfThrones.dragon+"@gmail.comm",
+    age: Faker::Number.within(range: 14..80),
+    city_id: rand(1..10)
+  )
+end
+
+10.times do
+  city = City.create!(
+    name: Faker::Movies::StarWars.planet,
+    zip_code: Faker::Address.zip_code
+  )
+end
+
+20.times do
+  gossip = Gossip.create!(
+    title: Faker::Music::Opera.mozart,
+    content: Faker::Music::Prince.lyric,
+    user_id: rand(1..10),
+    tag_id: rand(1..10)
+  )
+end
+
+puts "succesfully done"
